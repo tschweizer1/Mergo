3ac4d4f21bca2289731e41be5b691ba6fde6f320
gc <chavezagreyson@gmail.com>
2026-04-23 15:08:29 -0700

    Secret Stuff

--- Mergo/Player.cs
+++ Mergo/Player.cs
@@ -1,8 +1,8 @@
 ﻿public class Player
 {
     // TODO: branches will replace these 0 placeholders
-    public float MoveSpeed = 0f;
-    public float JumpForce = 0f;
+    public float MoveSpeed = 2564f;
+    public float JumpForce = 934587f;
 
     public void Move(float horizontal)
     {
