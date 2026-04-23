fb3778ded337f1b546edd5adfae8bd340a46e71e
a-zagski <zebrazag222@gmail.com>
2026-04-23 15:00:09 -0700

    player stats change

--- Mergo/Player.cs
+++ Mergo/Player.cs
@@ -1,8 +1,8 @@
 ﻿public class Player
 {
     // TODO: branches will replace these 0 placeholders
-    public float MoveSpeed = 0f;
-    public float JumpForce = 0f;
+    public float MoveSpeed = 3f;
+    public float JumpForce = 5f;
 
     public void Move(float horizontal)
     {
